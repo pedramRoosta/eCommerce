e674ed3f0efc6f46436c092444d4eac5d64ea8bc
pedram <pedram.roo@gmail.com>
2020-06-24 10:43:35 -0600

    completing homepage mockup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import { GalList } from './components/gal-list/gal-list';
-import { Searchbox } from './components/search-box/search-box';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      galleryItems: [],
-      searchTerm: ''
-    }
-  }
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users').then(Response => Response.json()).then(j => this.setState({ galleryItems: j }))
-  }
-  onChange = (e) => {
-    this.setState({ searchTerm: e.target.value })
-  }
-  render() {
-    const { galleryItems, searchTerm } = this.state;
-    const filtered = galleryItems.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
-    return (
-      <div className="App">
-        <h1 className="app-title">My First Profile</h1>
-        <Searchbox onChangefunc={this.onChange} placeHolder='Search items' />
-        <GalList galItems={filtered} />
-
-      </div>
-    );
-  }
-}
-
+import {HomePage} from './pages/homepage/homepage'
+const App=()=>(
+  <div className="app">
+    <HomePage/>
+  </div>
+)
 export default App;
--- src/components/search-box/search-box.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './search-box.css'
-
-export const Searchbox = ({onChangefunc,placeHolder}) => (
-
-    <div>
-        <input type="search" placeholder={placeHolder} className="searchBox" onChange={onChangefunc}/>
-    </div>
-)
